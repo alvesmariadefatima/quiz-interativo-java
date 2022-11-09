d8510d486f95d6f017ac28735bf0978edc9b17a2
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2022-11-09 08:47:50 -0300

    Modificação nos construtores da classe Perguntas

--- Perguntas.java
+++ Perguntas.java
@@ -3,9 +3,13 @@ package jogoquiz;
 public class Perguntas {
     String prompt;
     String resposta;
+    String dadosEmailUsuario;
+    String dadosSenhaUsuario;
     
     public Perguntas(String prompt, String resposta){
         this.prompt = prompt;
         this.resposta = resposta;
+        this.dadosEmailUsuario = dadosEmailUsuario;
+        this.dadosSenhaUsuario = dadosSenhaUsuario;
     }
 }
